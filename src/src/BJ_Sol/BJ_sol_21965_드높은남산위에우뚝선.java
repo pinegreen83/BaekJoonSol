f8637890d3ea0a947c8ed6bf53ae7c27b0645cd4
username <jjyoyo83@gmail.com>
2023-02-17 22:46:35 +0900

    21965 드높은 남산 위에 우뚝 선

---
+++ src/src/BJ_Sol/BJ_sol_21965_드높은남산위에우뚝선.java
@@ -0,0 +1,51 @@
+package src.BJ_Sol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_sol_21965_드높은남산위에우뚝선 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        boolean ans = true;
+        boolean rise = true;
+        int max = Integer.MIN_VALUE;
+        int now;
+        for(int i=0; i<N; i++) {
+            now = Integer.parseInt(st.nextToken());
+            if(rise) {
+                if(max < now) {
+                    max = now;
+                }
+                else if(max == now) {
+                    ans = false;
+                    break;
+                }
+                else if(max > now) {
+                    rise = false;
+                    max = now;
+                }
+            }
+            else {
+                if(max < now) {
+                    ans = false;
+                    break;
+                } else if (max > now) {
+                    max = now;
+                }
+            }
+        }
+
+        if(ans) System.out.println("YES");
+        else System.out.println("NO");
+    }
+
+}
